699c28bd8bc8078c72ff459e1fae8cf3e9a6eb6f
Gurleen Bamrah <gkbamrah@ualberta.ca>
2024-11-02 16:57:31 -0600

    added firebase code

--- Trojan0Project/app/src/main/java/com/example/trojan0project/BrowseProfileAdmin.java
+++ Trojan0Project/app/src/main/java/com/example/trojan0project/BrowseProfileAdmin.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,21 +13,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 import java.util.ArrayList;
 
 public class BrowseProfileAdmin extends MainActivity implements RemoveProfileFragment.RemoveProfileDialogListener {
 
     private ArrayList<Profile> dataList;
     private ListView profileList;
-    private ArrayAdapter<Profile> profileAdapter;
+    private ProfileAdapter profileAdapter;
+    private FirebaseFirestore db;
 
-    public void removeProfile(Profile profile) {
-        if (profile != null) {
-            profileAdapter.remove(profile);
-            profileAdapter.notifyDataSetChanged();
-        }
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,23 +32,12 @@ public class BrowseProfileAdmin extends MainActivity implements RemoveProfileFra
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_browse_profile_admin);
 
-
-        String[] profiles = {"Gurleen", "Farza", "Vishal", "Vivaan", "Charvi", "Jacob"};
-        int[] profilePicture = {R.drawable.baseline_person_24,
-                R.drawable.baseline_person_24,
-                R.drawable.baseline_person_24,
-                R.drawable.baseline_person_24,
-                R.drawable.baseline_person_24,
-                R.drawable.baseline_person_24};
-        dataList = new ArrayList<Profile>();
-        for (int i = 0; i < profiles.length; i++) {
-            dataList.add(new Profile(profiles[i], profilePicture[i]));
-        }
+        db = FirebaseFirestore.getInstance();
         profileList = findViewById(R.id.profile_list);
-
+        dataList = new ArrayList<>();
         profileAdapter = new ProfileAdapter(this, dataList);
-
         profileList.setAdapter(profileAdapter);
+        getProfile();
 
         profileList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -60,12 +47,48 @@ public class BrowseProfileAdmin extends MainActivity implements RemoveProfileFra
             }
         });
 
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
+    private void getProfile(){
+        db.collection("users")
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    dataList.clear();
+                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                        String username = document.getString("username");
+                        int profileImage = R.drawable.baseline_person_24;
+                        dataList.add(new Profile(username, profileImage));
+                    }
+                    profileAdapter.notifyDataSetChanged();
+                })
+                .addOnFailureListener(e -> {
+                    Toast.makeText(this, "Failed to get data", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    @Override
+    public void removeProfile(Profile profile){
+        db.collection("users")
+                .whereEqualTo("username", profile.getName())
+                .get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    for (QueryDocumentSnapshot document : queryDocumentSnapshots){
+                        db.collection("users").document(document.getId()).delete()
+                                .addOnSuccessListener(Void ->{
+                                    dataList.remove(profile);
+                                    profileAdapter.notifyDataSetChanged();
+                                    Toast.makeText(this, "Profile is deleted", Toast.LENGTH_SHORT).show();
+                                })
+                                .addOnFailureListener(e ->
+                                        Toast.makeText(this, "Profile not deleted", Toast.LENGTH_SHORT).show());
+
+                    }
+                })
+                .addOnFailureListener(e ->{
+                    Toast.makeText(this, "Profile not deleted", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+
+
 }
 

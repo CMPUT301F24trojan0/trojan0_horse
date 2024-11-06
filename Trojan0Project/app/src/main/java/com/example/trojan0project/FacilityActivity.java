2a84229bf966f20587738f3762073976bc2b6d6f
Farza Sipra <farza@ualberta.ca>
2024-11-05 17:07:47 -0700

    connected to fb

--- Trojan0Project/app/src/main/java/com/example/trojan0project/FacilityActivity.java
+++ Trojan0Project/app/src/main/java/com/example/trojan0project/FacilityActivity.java
@@ -5,11 +5,20 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
@@ -19,13 +28,36 @@ public class FacilityActivity extends AppCompatActivity implements DeleteFacilit
     private ArrayAdapter<Facility> facilityAdminAdapter;
     private ArrayList<Facility> dataList;
     private Facility selectedFacility = null;
+    private FirebaseFirestore db;
 
 
     //NEW
     @Override
     public void deleteFacility(Facility facility) {
         if (selectedFacility != null) { //city is not null so that means the user clicked on an existing city
-            facilityAdminAdapter.remove(selectedFacility);
+            //facilityAdminAdapter.remove(selectedFacility);
+            //facilityAdminAdapter.notifyDataSetChanged();
+            db.collection("organizers")
+                    .whereEqualTo("facilityName", selectedFacility.getFacilityName())
+                    .get()
+                    .addOnSuccessListener(queryDocumentSnapshots -> {
+                        for (QueryDocumentSnapshot document : queryDocumentSnapshots){
+                            db.collection("organizers").document(document.getId())
+                                    .update("facilityName", FieldValue.delete())
+                                    .addOnSuccessListener(Void ->{
+                                        dataList.remove(selectedFacility);
+                                        facilityAdminAdapter.notifyDataSetChanged();
+
+                                        Toast.makeText(this, "Facility is deleted", Toast.LENGTH_SHORT).show();
+                                    })
+                                    .addOnFailureListener(e ->
+                                            Toast.makeText(this, "Facility not deleted", Toast.LENGTH_SHORT).show());
+
+                        }
+                    })
+                    .addOnFailureListener(e ->{
+                        Toast.makeText(this, "Event not deleted", Toast.LENGTH_SHORT).show();
+                    });
             facilityAdminAdapter.notifyDataSetChanged();
         }
 
@@ -37,19 +69,44 @@ public class FacilityActivity extends AppCompatActivity implements DeleteFacilit
         EdgeToEdge.enable(this);
         setContentView(R.layout.facility_main);
 
+        db = FirebaseFirestore.getInstance();
+        final CollectionReference collectionReference = db.collection("organizers");
+
 
 
         String []facilities = {"Swimming Pool","Ice Rink", "Field", "Gym" }; //string array consisting of events which can be fed into ListView
 
         dataList = new ArrayList<Facility>(); // ArrayList which will contain the data (string array of events)
-        for (int i = 0; i < facilities.length; i++) {
-            dataList.add(new Facility(facilities[i]));
+        //for (int i = 0; i < facilities.length; i++) {
+            //dataList.add(new Facility(facilities[i]));
 
-        }
+        //}
         //dataList.addAll(Arrays.asList(events)); // add the data in string array to dataList
         facilityAdminList = findViewById(R.id.admin_facilities_list); //find reference to to the ListView and assign it to eventAdminList
         facilityAdminAdapter = new FacilityArrayAdapter(this, dataList); // link content file and  and datalist as well as pass id of textview in content.xml
         facilityAdminList.setAdapter(facilityAdminAdapter); // show each TextView in scrolling list form
+
+
+        // Listener for Firestore data
+        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot queryDocumentSnapshots,@Nullable FirebaseFirestoreException error) {
+
+
+                dataList.clear(); // Clear the existing data
+
+                for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                    String facilityName = (String) doc.getData().get("facilityName");
+
+                    dataList.add(new Facility(facilityName));
+
+                }
+
+                facilityAdminAdapter.notifyDataSetChanged();
+            }
+        });
+
+
 
         facilityAdminList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
